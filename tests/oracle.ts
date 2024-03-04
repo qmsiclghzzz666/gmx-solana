809600c77032e66054cdb6d5ce6feeb23ca1c8d5
orion-047 <161582484+orion-047@users.noreply.github.com>
2024-03-05 04:16:35 +0800

    add the IDL of chainlink store

--- tests/oracle.ts
+++ tests/oracle.ts
@@ -1,14 +1,43 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Oracle } from "../target/types/oracle";
+import { IDL as chainlinkIDL } from "../external-programs/chainlink-store";
+import { getProvider } from "../utils/fixtures";
 
 describe("oracle", () => {
+    const provider = getProvider();
+
+    const chainlinkID = "HEvSKofvBgfaexv23kMabbYqxasxU3mQ4ibBMEmJWHny";
     const oracle = anchor.workspace.Oracle as anchor.Program<Oracle>;
+    const chainlink = new anchor.Program(chainlinkIDL, chainlinkID);
+
+    const mockFeedAccount = anchor.web3.Keypair.generate();
 
     it("should work", async () => {
-        const round = await oracle.methods.initialize().accounts({
-            feed: "Cv4T27XbjVoKUYwP72NQQanvZeA7W4YF9L4EnYT9kx5o",
-            chainlink: "HEvSKofvBgfaexv23kMabbYqxasxU3mQ4ibBMEmJWHny",
-        }).view();
-        console.log(`got round of slot ${round.slot}, answer: ${round.answer}, feed ts: ${round.timestamp}, sys ts: ${round.sysTimestamp}`, round);
+        try {
+            const round = await oracle.methods.initialize().accounts({
+                feed: "Cv4T27XbjVoKUYwP72NQQanvZeA7W4YF9L4EnYT9kx5o",
+                chainlink: chainlinkID,
+            }).view();
+            console.log(`got round of slot ${round.slot}, answer: ${round.answer}, feed ts: ${round.timestamp}, sys ts: ${round.sysTimestamp}`, round);
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+
+        try {
+            const createFeedTx = await chainlink.methods.createFeed("FOO", 1, 2, 3).accounts({
+                feed: mockFeedAccount.publicKey,
+                authority: provider.wallet.publicKey,
+            }).signers([mockFeedAccount]).preInstructions([
+                await chainlink.account.transmissions.createInstruction(
+                    mockFeedAccount,
+                    8 + 192 + (3 + 3) * 48
+                ),
+            ]).rpc();
+            console.log("create feed:", createFeedTx);
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
     });
 });
