9a592d58ade32f091431ee5d5e0fbc08a8e21000
orion-047 <161582484+orion-047@users.noreply.github.com>
2024-03-04 23:27:23 +0800

    oracle: get slot ts from sysvar

--- programs/oracle/src/lib.rs
+++ programs/oracle/src/lib.rs
@@ -6,13 +6,41 @@ declare_id!("8LmVjFpoR6hupp6WZZb6EbmupaXvivaCEk2iAHskr1en");
 pub mod oracle {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<i128> {
+    pub fn initialize(ctx: Context<Initialize>) -> Result<Round> {
         let round = chainlink_solana::latest_round_data(
             ctx.accounts.chainlink.to_account_info(),
             ctx.accounts.feed.to_account_info(),
         )?;
         msg!("answer: {}", round.answer);
-        Ok(round.answer)
+        Round::try_new(round)
+    }
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize)]
+pub struct Round {
+    pub round_id: u32,
+    pub slot: u64,
+    pub timestamp: u32,
+    pub answer: i128,
+    pub sys_timestamp: i64,
+}
+
+impl Round {
+    fn try_new(round: chainlink_solana::Round) -> Result<Self> {
+        let clock = Clock::get()?;
+        let chainlink_solana::Round {
+            round_id,
+            slot,
+            timestamp,
+            answer,
+        } = round;
+        Ok(Self {
+            round_id,
+            slot,
+            timestamp,
+            answer,
+            sys_timestamp: clock.unix_timestamp,
+        })
     }
 }
 
--- tests/oracle.ts
+++ tests/oracle.ts
@@ -5,10 +5,10 @@ describe("oracle", () => {
     const oracle = anchor.workspace.Oracle as anchor.Program<Oracle>;
 
     it("should work", async () => {
-        const answer = await oracle.methods.initialize().accounts({
+        const round = await oracle.methods.initialize().accounts({
             feed: "Cv4T27XbjVoKUYwP72NQQanvZeA7W4YF9L4EnYT9kx5o",
             chainlink: "HEvSKofvBgfaexv23kMabbYqxasxU3mQ4ibBMEmJWHny",
-        }).view() as anchor.BN;
-        console.log(`got answer: ${answer}`);
+        }).view();
+        console.log(`got round of slot ${round.slot}, answer: ${round.answer}, feed ts: ${round.timestamp}, sys ts: ${round.sysTimestamp}`, round);
     });
 });
