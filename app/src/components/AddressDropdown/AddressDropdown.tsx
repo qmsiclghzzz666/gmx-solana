46e7f67ad68f22a97e7a2c3393ecb8bd409f812f
orion-047 <161582484+orion-047@users.noreply.github.com>
2024-04-25 03:32:47 +0800

    app: use custom connect button and drawdown

---
+++ app/src/components/AddressDropdown/AddressDropdown.tsx
@@ -0,0 +1,102 @@
+import { Menu } from "@headlessui/react";
+import { Trans } from "@lingui/macro";
+import copy from "@/img/ic_copy_20.svg";
+import disconnect from "@/img/ic_sign_out_20.svg";
+import { FaChevronDown } from "react-icons/fa";
+import { createBreakpoint, useCopyToClipboard } from "react-use";
+import "./AddressDropdown.scss";
+import { PublicKey } from "@solana/web3.js";
+
+type Props = {
+  account: PublicKey;
+  // accountUrl: string;
+  disconnectAccountAndCloseSettings: () => void;
+};
+
+function shortenAddress(address: string, length: number, padStart: number = 1) {
+  if (!length) {
+    return "";
+  }
+  if (!address) {
+    return address;
+  }
+  if (address.length < 10) {
+    return address;
+  }
+  if (length >= address.length) {
+    return address;
+  }
+  const left = Math.floor((length - 3) / 2) + (padStart || 0);
+  return address.substring(0, left) + "..." + address.substring(address.length - (length - (left + 3)), address.length);
+}
+
+function AddressDropdown({ account, disconnectAccountAndCloseSettings }: Props) {
+  const useBreakpoint = createBreakpoint({ L: 600, M: 550, S: 400 });
+  const breakpoint = useBreakpoint();
+  const [, copyToClipboard] = useCopyToClipboard();
+  // const { ensName } = useENS(account);
+  // const { provider: ethereumProvider } = useJsonRpcProvider(ETH_MAINNET);
+  const displayAddressLength = breakpoint === "S" ? 9 : 13;
+  // const [, setOneClickModalOpen] = useSubaccountModalOpen();
+  // const handleSubaccountClick = useCallback(() => {
+  //   setOneClickModalOpen(true);
+  // }, [setOneClickModalOpen]);
+
+  return (
+    <Menu>
+      <Menu.Button as="div">
+        <button className="App-cta small transparent address-btn">
+          {/* <div className="user-avatar">
+            {ethereumProvider && <Davatar size={20} address={account} provider={ethereumProvider} />}
+          </div> */}
+          <span className="user-address">{shortenAddress(account.toBase58(), displayAddressLength)}</span>
+          <FaChevronDown />
+        </button>
+      </Menu.Button>
+      <div>
+        <Menu.Items as="div" className="menu-items">
+          <Menu.Item>
+            <div
+              className="menu-item"
+              onClick={() => {
+                copyToClipboard(account.toBase58());
+                // helperToast.success(t`Address copied to your clipboard`);
+              }}
+            >
+              <img width={20} src={copy} alt="Copy user address" />
+              <p>
+                <Trans>Copy Address</Trans>
+              </p>
+            </div>
+          </Menu.Item>
+          {/* <Menu.Item>
+            <ExternalLink href={accountUrl} className="menu-item">
+              <img width={20} src={externalLink} alt="Open address in explorer" />
+              <p>
+                <Trans>View in Explorer</Trans>
+              </p>
+            </ExternalLink>
+          </Menu.Item> */}
+          {/* <Menu.Item>
+            <div className="menu-item" onClick={handleSubaccountClick}>
+              <img width={20} src={oneClickTradingIcon} alt="Open One-click Trading settings" />
+              <p>
+                <Trans>One-Click Trading</Trans>
+              </p>
+            </div>
+          </Menu.Item> */}
+          <Menu.Item>
+            <div className="menu-item" onClick={disconnectAccountAndCloseSettings}>
+              <img width={20} src={disconnect} alt="Disconnect the wallet" />
+              <p>
+                <Trans>Disconnect</Trans>
+              </p>
+            </div>
+          </Menu.Item>
+        </Menu.Items>
+      </div>
+    </Menu>
+  );
+}
+
+export default AddressDropdown;
--- app/src/components/Header/AppHeaderUser.tsx
+++ app/src/components/Header/AppHeaderUser.tsx
@@ -3,16 +3,25 @@ import cx from "classnames";
 
 import "./Header.scss";
 import { HeaderLink } from "./HeaderLink";
-import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { useWalletModal } from "@solana/wallet-adapter-react-ui";
+import connectWalletImg from "@/img/ic_wallet_24.svg";
 
 import "@solana/wallet-adapter-react-ui/styles.css";
+import { useWallet } from "@solana/wallet-adapter-react";
+import ConnectWalletButton from "../Common/ConnectWalletButton/ConnectWalletButton";
+import AddressDropdown from "../AddressDropdown/AddressDropdown";
+import { useCallback } from "react";
 
 interface Props {
   small?: boolean;
 }
 
 export function AppHeaderUser({ small }: Props) {
-  void small;
+  const wallet = useWallet();
+  const { setVisible } = useWalletModal();
+  const disconnectAccountAndCloseSettings = useCallback(() => {
+    void wallet.disconnect();
+  }, [wallet]);
   return (
     <div className="App-header-user">
       <div className={cx("App-header-trade-link")}>
@@ -21,14 +30,26 @@ export function AppHeaderUser({ small }: Props) {
         </HeaderLink>
       </div>
       <>
-        <WalletMultiButton />
+        {(wallet.connected && wallet.publicKey) ? (
+          <div className="App-header-user-address">
+            <AddressDropdown
+              account={wallet.publicKey}
+              disconnectAccountAndCloseSettings={disconnectAccountAndCloseSettings}
+            />
+          </div>
+        ) : (
+          <ConnectWalletButton onClick={() => setVisible(true)} imgSrc={connectWalletImg}>
+            {small ? <Trans>Connect</Trans> : <Trans>Connect Wallet</Trans>}
+          </ConnectWalletButton>
+        )
+        }
         {/* <NetworkDropdown
-          small={small}
-          networkOptions={NETWORK_OPTIONS}
-          selectorLabel={selectorLabel}
-          openSettings={openSettings}
-        /> */}
+        small={small}
+        networkOptions={NETWORK_OPTIONS}
+        selectorLabel={selectorLabel}
+        openSettings={openSettings}
+      /> */}
       </>
-    </div>
+    </div >
   );
 }
