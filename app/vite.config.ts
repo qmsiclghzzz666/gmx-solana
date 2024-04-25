3eb3d5228814a01bb26e7c1c34bcebda20e0cd3f
orion-047 <161582484+orion-047@users.noreply.github.com>
2024-04-25 23:48:49 +0800

    app: add HTTPS options

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import path from "path";
 import { loadGMSOLDeployment } from "./utils/load-deployment";
+import { loadHttpsOptions } from './utils/load-https-options';
 
 export default defineConfig(async ({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
@@ -25,6 +26,9 @@ export default defineConfig(async ({ mode }) => {
     },
     define: {
       __GMSOL_DEPLOYMENT__: await loadGMSOLDeployment(env.GMSOL_DEPLOYMENT ? path.resolve(__dirname, env.GMSOL_DEPLOYMENT) : undefined),
+    },
+    server: {
+      https: await loadHttpsOptions(env.GMSOL_SSL_DIR ? path.resolve(__dirname, env.GMSOL_SSL_DIR) : undefined),
     }
   }
 })
