cd5b023859e40f051c5fd3e4c3d8d47b62e578e9
orion-047 <161582484+orion-047@users.noreply.github.com>
2024-04-25 16:31:39 +0800

    app: better connect wallet button

--- app/src/components/Common/ConnectWalletButton/ConnectWalletButton.tsx
+++ app/src/components/Common/ConnectWalletButton/ConnectWalletButton.tsx
@@ -1,17 +1,55 @@
-import { ReactNode } from "react";
+import { useCallback, useState } from "react";
 import "./ConnectWalletButton.scss";
+import connectWalletImg from "@/img/ic_wallet_24.svg";
+import { Trans } from "@lingui/macro";
+import { MdOutlineStopCircle } from "react-icons/md";
+import cx from "classnames";
 
 type Props = {
-  imgSrc: string;
-  children: ReactNode;
-  onClick: () => void;
+  small?: boolean,
+  onConnect: () => void;
+  onCancel?: () => void;
+  connecting: boolean,
+  disconnecting: boolean,
 };
 
-export default function ConnectWalletButton({ imgSrc, children, onClick }: Props) {
+export default function ConnectWalletButton({
+  small,
+  onConnect,
+  onCancel,
+  connecting,
+  disconnecting,
+}: Props) {
+  const [hover, setHover] = useState(false);
+
+  const handleClick = useCallback(() => {
+    if (!connecting && !disconnecting) {
+      onConnect();
+    } else if (connecting && !disconnecting) {
+      if (onCancel) {
+        onCancel();
+      }
+    }
+  }, [connecting, disconnecting, onCancel, onConnect]);
+
   return (
-    <button className="connect-wallet-btn" onClick={onClick}>
-      {imgSrc && <img className="btn-icon" src={imgSrc} alt="Connect Wallet" />}
-      <span className="btn-label">{children}</span>
+    <button
+      className={cx("connect-wallet-btn", {
+        "connect-wallet-btn-connecting": connecting,
+      })}
+      onClick={handleClick}
+      disabled={disconnecting}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
+      {!(connecting && hover) && <img className="btn-icon" src={connectWalletImg} alt="Connect Wallet" />}
+      {(connecting && hover) && <MdOutlineStopCircle size={18} />}
+      <span className="btn-label">{
+        connecting ? <Trans>Connecting...</Trans> :
+          disconnecting ? <Trans>Disconnecting...</Trans> :
+            small ? <Trans>Connect</Trans> :
+              <Trans>Connect Wallet</Trans>
+      }</span>
     </button>
   );
 }
--- app/src/components/Header/AppHeaderUser.tsx
+++ app/src/components/Header/AppHeaderUser.tsx
@@ -3,7 +3,6 @@ import cx from "classnames";
 
 import "./Header.scss";
 import { HeaderLink } from "./HeaderLink";
-import connectWalletImg from "@/img/ic_wallet_24.svg";
 
 import "@solana/wallet-adapter-react-ui/styles.css";
 import { useWallet } from "@solana/wallet-adapter-react";
@@ -18,6 +17,7 @@ interface Props {
 
 export function AppHeaderUser({ small }: Props) {
   const wallet = useWallet();
+  const { connected, connecting, disconnecting, publicKey } = wallet;
   const openConnectModal = useOpenConnectModal();
   const disconnectAccountAndCloseSettings = useCallback(() => {
     void wallet.disconnect();
@@ -30,17 +30,21 @@ export function AppHeaderUser({ small }: Props) {
         </HeaderLink>
       </div>
       <>
-        {(wallet.connected && wallet.publicKey) ? (
+        {(connected && publicKey) ? (
           <div className="App-header-user-address">
             <AddressDropdown
-              account={wallet.publicKey}
+              account={publicKey}
               disconnectAccountAndCloseSettings={disconnectAccountAndCloseSettings}
             />
           </div>
         ) : (
-          <ConnectWalletButton onClick={openConnectModal} imgSrc={connectWalletImg}>
-            {small ? <Trans>Connect</Trans> : <Trans>Connect Wallet</Trans>}
-          </ConnectWalletButton>
+          <ConnectWalletButton
+            small={small}
+            onConnect={openConnectModal}
+            onCancel={disconnectAccountAndCloseSettings}
+            connecting={connecting}
+            disconnecting={disconnecting}
+          />
         )
         }
         {/* <NetworkDropdown
