fbddbbc4128b20f161cdc40b0a60e3695d170c6b
orion-047 <161582484+orion-047@users.noreply.github.com>
2024-06-07 01:52:30 +0800

    app: fix market fields

--- app/src/onchain/market/use-markets.ts
+++ app/src/onchain/market/use-markets.ts
@@ -31,8 +31,8 @@ export const useMarkets = (params?: { store: PublicKey, marketTokens: PublicKey[
           indexTokenAddress: market.meta.indexTokenMint,
           longTokenAddress: market.meta.longTokenMint,
           shortTokenAddress: market.meta.shortTokenMint,
-          longPoolAmount: isSingle ? market.pools.pools[0].longTokenAmount.div(BN_TWO) : market.pools.pools[0].longTokenAmount,
-          shortPoolAmount: isSingle ? market.pools.pools[0].longTokenAmount.div(BN_TWO) : market.pools.pools[0].shortTokenAmount,
+          longPoolAmount: isSingle ? market.pools.values[0].longTokenAmount.div(BN_TWO) : market.pools.values[0].longTokenAmount,
+          shortPoolAmount: isSingle ? market.pools.values[0].longTokenAmount.div(BN_TWO) : market.pools.values[0].shortTokenAmount,
           isSingle,
         };
         return data;
