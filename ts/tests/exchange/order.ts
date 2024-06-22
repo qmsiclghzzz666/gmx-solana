301d9ec6f1c78f0e2fd451efa208845cd5e87e8d
orion-047 <161582484+orion-047@users.noreply.github.com>
2024-06-22 14:25:24 +0800

    add test for decreasing position without swap

--- ts/tests/exchange/order.ts
+++ ts/tests/exchange/order.ts
@@ -137,4 +137,109 @@ describe("exchange: order", () => {
             throw error;
         }
     });
+
+    it("decrease position without swap", async () => {
+        const recentTimestamp = toInteger(Date.now() / 1000);
+        // Increase position.
+        let increaseOrder: PublicKey;
+        try {
+            const [signature, address] = await invokeCreateIncreaseOrderWithPayerAsSigner(exchange, {
+                store: dataStoreAddress,
+                payer: user0,
+                marketToken: GMFakeFakeUsdG,
+                collateralToken: fakeTokenMint,
+                initialCollateralDeltaAmount: 400_000_000,
+                isLong: true,
+                sizeDeltaUsd: 200_000_000_000_000_000_000n,
+                options: {
+                    initialCollateralToken: usdGTokenMint,
+                    swapPath: [
+                        GMFakeFakeUsdG
+                    ],
+                    hint: {
+                        longToken: fakeTokenMint,
+                        shortToken: usdGTokenMint,
+                    },
+                    tokenMap,
+                }
+            }, {
+                computeUnits: 400_000,
+            });
+            increaseOrder = address;
+            console.log(`order ${increaseOrder} created at ${signature}`);
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+        try {
+            const signature = await executeOrder(false, provider.connection, {
+                authority: signer0,
+                store: dataStoreAddress,
+                oracle: oracleAddress,
+                order: increaseOrder,
+                recentTimestamp,
+                holding: dataStore.provider.publicKey,
+                options: {
+                    executionFee: 5001,
+                }
+            }, {
+                computeUnits: 400_000,
+            });
+            console.log(`order ${increaseOrder} executed at ${signature}`);
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+
+        // Decrease position.
+        let decreaseOrder: PublicKey;
+        try {
+            const [position] = findPositionPDA(dataStoreAddress, user0.publicKey, GMFakeFakeUsdG, fakeTokenMint, true);
+            const [signature, address] = await invokeCreateDecreaseOrderWithPayerAsSigner(exchange, {
+                store: dataStoreAddress,
+                payer: user0,
+                position,
+                initialCollateralDeltaAmount: 0,
+                sizeDeltaUsd: 200_000_000_000_000_000_000n,
+                options: {
+                    finalOutputToken: fakeTokenMint,
+                    swapPath: [],
+                    hint: {
+                        market: {
+                            marketToken: GMFakeFakeUsdG,
+                            longToken: fakeTokenMint,
+                            shortToken: usdGTokenMint,
+                        },
+                        collateralToken: fakeTokenMint,
+                        isLong: true,
+                    },
+                    tokenMap,
+                }
+            });
+            decreaseOrder = address;
+            console.log(`order ${decreaseOrder} created at ${signature}`);
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+        try {
+            const signature = await executeOrder(false, provider.connection, {
+                authority: signer0,
+                store: dataStoreAddress,
+                oracle: oracleAddress,
+                order: decreaseOrder,
+                recentTimestamp,
+                holding: dataStore.provider.publicKey,
+                options: {
+                    executionFee: 5001,
+                }
+            }, {
+                computeUnits: 800_000,
+            });
+            console.log(`order ${decreaseOrder} executed at ${signature}`);
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    });
 });
