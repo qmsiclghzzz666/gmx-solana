bdc9f8d48f9ceaaa9c6ff4054598b632519116cd
orion-047 <161582484+orion-047@users.noreply.github.com>
2024-06-26 18:40:43 +0800

    sdk: re-export CPI events

--- crates/gmsol/src/types.rs
+++ crates/gmsol/src/types.rs
@@ -1,2 +1,6 @@
-pub use data_store::{instruction as store_instruction, states::*};
+pub use data_store::{
+    instruction as store_instruction,
+    instructions::{RemoveDepositEvent, RemoveOrderEvent, RemoveWithdrawalEvent},
+    states::*,
+};
 pub use exchange::instruction as exchange_instruction;
