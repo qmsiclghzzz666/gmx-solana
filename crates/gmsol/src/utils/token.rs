3181e5c21540977fe85c77ad78e8a392baedd7fb
orion-047 <161582484+orion-047@users.noreply.github.com>
2024-08-06 21:37:11 +0800

    sdk: fix rounding strategy

--- crates/gmsol/src/utils/token.rs
+++ crates/gmsol/src/utils/token.rs
@@ -14,12 +14,11 @@ pub fn price_to_min_output_amount(
 ) -> Option<u64> {
     let token_in = token_map.get(token_in)?;
     let token_in_amount = unsigned_amount_to_decimal(token_in_amount, token_in.token_decimals());
-    let mut token_out_amount = token_in_amount.checked_div(token_in_to_token_out_price)?;
+    let token_out_amount = token_in_amount.checked_div(token_in_to_token_out_price)?;
     let token_out = token_map.get(token_out)?;
     let scale = token_out.token_decimals().into();
-    token_out_amount.rescale(scale);
     token_out_amount
-        .round_dp_with_strategy(scale, RoundingStrategy::ToPositiveInfinity)
+        .round_dp_with_strategy(scale, RoundingStrategy::AwayFromZero)
         .mantissa()
         .try_into()
         .ok()
