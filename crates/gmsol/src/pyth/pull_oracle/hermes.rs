7c6e8bb09155bcd49ee0c615899b71ce3705093d
orion-047 <161582484+orion-047@users.noreply.github.com>
2024-08-25 14:14:49 +0800

    pyth: impl Clone for BinaryPriceUpdate

--- crates/gmsol/src/pyth/pull_oracle/hermes.rs
+++ crates/gmsol/src/pyth/pull_oracle/hermes.rs
@@ -155,7 +155,7 @@ fn deserialize_price_update_event(event: &eventsource_stream::Event) -> crate::R
 }
 
 /// Price Update.
-#[derive(Debug, serde::Serialize, serde::Deserialize)]
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
 pub struct PriceUpdate {
     pub(crate) binary: BinaryPriceUpdate,
     #[serde(default)]
@@ -182,7 +182,7 @@ impl PriceUpdate {
     }
 }
 
-#[derive(Debug, serde::Serialize, serde::Deserialize)]
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
 pub struct BinaryPriceUpdate {
     pub(crate) encoding: EncodingType,
     pub(crate) data: Vec<String>,
