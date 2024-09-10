e214f505f50d17932955f67d2d9c4f422c84e763
orion-047 <161582484+orion-047@users.noreply.github.com>
2024-09-10 22:56:32 +0800

    test: fund the used accounts

--- crates/gmsol/tests/anchor_tests/deposit.rs
+++ crates/gmsol/tests/anchor_tests/deposit.rs
@@ -4,6 +4,20 @@ use crate::anchor_tests::setup;
 async fn create_deposit() -> eyre::Result<()> {
     let deployment = setup::current_deployment().await?;
 
+    let _guard = deployment.use_accounts().await?;
+
     tracing::info!("hello: {deployment:#?}");
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn create_deposit_2() -> eyre::Result<()> {
+    let deployment = setup::current_deployment().await?;
+
+    let _guard = deployment.use_accounts().await?;
+
+    tracing::info!("hello 2: {deployment:#?}");
+
     Ok(())
 }
--- crates/gmsol/tests/anchor_tests/setup.rs
+++ crates/gmsol/tests/anchor_tests/setup.rs
@@ -1,16 +1,27 @@
-use std::fmt;
+use std::{
+    fmt,
+    future::Future,
+    sync::{
+        atomic::{AtomicBool, AtomicUsize, Ordering},
+        Arc,
+    },
+    time::Duration,
+};
 
 use anchor_client::{
     solana_sdk::{
+        commitment_config::CommitmentConfig,
         pubkey::Pubkey,
         signature::Keypair,
         signer::{EncodableKey, Signer},
+        system_instruction,
     },
     Cluster,
 };
+use event_listener::Event;
 use gmsol::{
-    utils::{shared_signer, SignerRef},
-    Client,
+    utils::{shared_signer, SignerRef, TransactionBuilder},
+    Client, ClientOptions,
 };
 use tokio::sync::OnceCell;
 use tracing::level_filters::LevelFilter;
@@ -22,8 +33,6 @@ pub struct Deployment {
     pub client: Client<SignerRef>,
     /// Users.
     pub users: Users,
-    /// Keepers.
-    pub keepers: Keepers,
     /// Store.
     pub store: Pubkey,
     /// Token Map.
@@ -38,7 +47,6 @@ impl fmt::Debug for Deployment {
             .field("cluster", self.client.cluster())
             .field("payer", &self.client.payer())
             .field("users", &self.users)
-            .field("keepers", &self.keepers)
             .field("store", &self.store)
             .field("token_map", &self.token_map.pubkey())
             .field("oracle", &self.oracle)
@@ -53,7 +61,6 @@ impl Deployment {
         Ok(Self {
             client,
             users: Default::default(),
-            keepers: Default::default(),
             store,
             token_map: Keypair::new(),
             oracle,
@@ -93,12 +100,15 @@ impl Deployment {
             })
             .unwrap_or_default();
 
-        let client = Client::new(
+        let client = Client::new_with_options(
             endpoint.parse().map_err(eyre::Error::msg)?,
             shared_signer(
                 Keypair::read_from_file(&*wallet)
                     .map_err(|err| eyre::Error::msg(err.to_string()))?,
             ),
+            ClientOptions::builder()
+                .commitment(CommitmentConfig::confirmed())
+                .build(),
         )?;
         let store = client.find_store_address(&store_key);
         Ok((client, store))
@@ -108,7 +118,7 @@ impl Deployment {
         tracing_subscriber::fmt()
             .with_env_filter(
                 EnvFilter::builder()
-                    .with_default_directive(LevelFilter::INFO.into())
+                    .with_default_directive(LevelFilter::ERROR.into())
                     .from_env_lossy(),
             )
             .try_init()
@@ -117,6 +127,102 @@ impl Deployment {
     }
 
     async fn setup(&self) -> eyre::Result<()> {
+        tracing::info!("[Setting up everything...]");
+        let _guard = self.use_accounts().await?;
+
+        Ok(())
+    }
+
+    pub(crate) async fn use_accounts(&self) -> eyre::Result<Guard> {
+        let guard = self.users.use_accounts();
+
+        if self
+            .users
+            .funded
+            .compare_exchange(false, true, Ordering::SeqCst, Ordering::SeqCst)
+            .is_ok()
+        {
+            self.fund_users().await?;
+        }
+
+        Ok(guard)
+    }
+
+    async fn fund_users(&self) -> eyre::Result<()> {
+        const LAMPORTS: u64 = 4_000_000_000;
+
+        let client = self.client.data_store().async_rpc();
+        let payer = self.client.payer();
+        let lamports = client.get_balance(&payer).await?;
+        tracing::info!(%payer, "before funding users: {lamports}");
+
+        let mut builder = TransactionBuilder::new(client);
+        builder.try_push_many(
+            self.users
+                .users()
+                .into_iter()
+                .inspect(|user| tracing::debug!(%user, "funding user with lamports {LAMPORTS}"))
+                .map(|user| system_instruction::transfer(&payer, &user, LAMPORTS))
+                .map(|ix| self.client.data_store_rpc().pre_instruction(ix)),
+            false,
+        )?;
+
+        match builder.send_all().await {
+            Ok(signatures) => {
+                tracing::debug!("funded users with {signatures:#?}");
+            }
+            Err((signatures, err)) => {
+                tracing::error!(%err, "failed to fund users, successful txns: {signatures:#?}");
+            }
+        }
+
+        Ok(())
+    }
+
+    async fn refund_payer(&self) -> eyre::Result<()> {
+        let client = self.client.data_store().async_rpc();
+        let payer = self.client.payer();
+
+        let mut builder = TransactionBuilder::new(self.client.data_store().async_rpc());
+
+        for user in self.users.keypairs() {
+            let lamports = client.get_balance(&user.pubkey()).await?;
+            if lamports == 0 {
+                continue;
+            }
+            tracing::debug!(user = %user.pubkey(), %lamports, "refund from user");
+            let ix = system_instruction::transfer(&user.pubkey(), &payer, lamports);
+            builder
+                .try_push(
+                    self.client
+                        .data_store_rpc()
+                        .signer(user)
+                        .pre_instruction(ix),
+                )
+                .map_err(|(_, err)| err)?;
+        }
+
+        match builder.send_all().await {
+            Ok(signatures) => {
+                tracing::debug!("refunded the payer with {signatures:#?}");
+            }
+            Err((signatures, err)) => {
+                tracing::error!(%err, "failed to refund the payer, successful txns: {signatures:#?}");
+            }
+        }
+
+        self.users.funded.store(false, Ordering::SeqCst);
+
+        let lamports = client.get_balance(&payer).await?;
+        tracing::info!(%payer, "after refunding the payer: {lamports}");
+        Ok(())
+    }
+
+    pub(crate) async fn refund_payer_when_not_in_use(&self, wait: Duration) -> eyre::Result<()> {
+        tokio::time::sleep(wait).await;
+        self.users.wait_until_not_in_use().await;
+        tracing::info!("[Cleanup...]");
+        self.refund_payer().await?;
         Ok(())
     }
 }
@@ -125,12 +231,18 @@ impl Deployment {
 pub struct Users {
     /// User 0.
     pub user_0: Keypair,
+    /// Keeper 0.
+    pub keeper_0: Keypair,
+    funded: Arc<AtomicBool>,
+    used: Arc<AtomicUsize>,
+    event: Arc<Event>,
 }
 
 impl fmt::Debug for Users {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_struct("Users")
             .field("user_0", &self.user_0.pubkey())
+            .field("keepr_0", &self.keeper_0.pubkey())
             .finish()
     }
 }
@@ -139,35 +251,84 @@ impl Default for Users {
     fn default() -> Self {
         Self {
             user_0: Keypair::new(),
-        }
-    }
-}
-
-/// Keepers.
-pub struct Keepers {
-    /// Keeper 0
-    pub keeper_0: Keypair,
-}
-
-impl fmt::Debug for Keepers {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.debug_struct("Keepers")
-            .field("keeper_0", &self.keeper_0.pubkey())
-            .finish()
-    }
-}
-
-impl Default for Keepers {
-    fn default() -> Self {
-        Self {
             keeper_0: Keypair::new(),
+            funded: Arc::new(AtomicBool::new(false)),
+            used: Arc::new(AtomicUsize::new(0)),
+            event: Arc::new(Event::new()),
         }
+    }
+}
+
+impl Users {
+    fn use_accounts(&self) -> Guard {
+        self.used.fetch_add(1, Ordering::SeqCst);
+        self.event.notify(usize::MAX);
+        Guard {
+            used: self.used.clone(),
+            event: self.event.clone(),
+        }
+    }
+
+    fn wait_until_not_in_use(&self) -> impl Future<Output = ()> {
+        let used = self.used.clone();
+        let event = self.event.clone();
+
+        async move {
+            loop {
+                if used.load(Ordering::SeqCst) == 0 {
+                    break;
+                }
+
+                let listener = event.listen();
+
+                if used.load(Ordering::SeqCst) == 0 {
+                    break;
+                }
+
+                listener.await;
+            }
+        }
+    }
+
+    fn users(&self) -> impl IntoIterator<Item = Pubkey> {
+        [self.user_0.pubkey(), self.keeper_0.pubkey()]
+    }
+
+    fn keypairs(&self) -> impl IntoIterator<Item = &Keypair> {
+        [&self.user_0, &self.keeper_0]
+    }
+}
+
+#[must_use]
+pub(crate) struct Guard {
+    used: Arc<AtomicUsize>,
+    event: Arc<Event>,
+}
+
+impl Drop for Guard {
+    fn drop(&mut self) {
+        self.used.fetch_sub(1, Ordering::SeqCst);
+        self.event.notify(usize::MAX);
     }
 }
 
 /// Get current deployment.
 pub async fn current_deployment() -> eyre::Result<&'static Deployment> {
     static DEPLOYMENT: OnceCell<Deployment> = OnceCell::const_new();
-
     DEPLOYMENT.get_or_try_init(Deployment::init).await
+}
+
+#[tokio::test]
+async fn refund_payer() -> eyre::Result<()> {
+    let wait = std::env::var("GMSOL_REFUND_WAIT")
+        .ok()
+        .and_then(|wait| wait.parse().ok())
+        .unwrap_or(1);
+    let deployment = current_deployment().await?;
+
+    deployment
+        .refund_payer_when_not_in_use(Duration::from_secs(wait))
+        .await?;
+
+    Ok(())
 }
