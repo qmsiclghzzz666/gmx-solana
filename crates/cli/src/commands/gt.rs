a18373fdc254221fcee639dc774ce85e6a0b5812
orion-047 <161582484+orion-047@users.noreply.github.com>
2025-06-20 02:47:49 +0000

    feat(cli): add commands to display GT related accounts

--- crates/cli/src/commands/gt.rs
+++ crates/cli/src/commands/gt.rs
@@ -1,11 +1,21 @@
 use crate::config::DisplayOptions;
+use eyre::OptionExt;
 use gmsol_sdk::{
-    ops::{gt::GtOps, treasury::TreasuryOps},
-    programs::anchor_lang::prelude::Pubkey,
+    core::{gt::GtBankFlags, pubkey::optional_address, token_config::TokenMapAccess},
+    ops::{
+        gt::{current_time_window_index, GtOps},
+        treasury::TreasuryOps,
+    },
+    programs::{
+        anchor_lang::prelude::Pubkey,
+        gmsol_store::accounts::GtExchangeVault,
+        gmsol_treasury::accounts::{Config, GtBank},
+    },
     serde::StringPubkey,
-    utils::{unsigned_amount_to_decimal, Amount},
+    solana_utils::solana_sdk::signer::Signer,
+    utils::{unsigned_amount_to_decimal, zero_copy::ZeroCopy, Amount},
 };
-use std::num::NonZeroU32;
+use std::{num::NonZeroU32, ops::Deref};
 
 /// GT-related commands.
 #[derive(Debug, clap::Args)]
@@ -64,6 +74,18 @@ enum Command {
         /// Whether to prepare the exchange vault before the request or not.
         #[arg(long, requires = "request")]
         prepare_vault: bool,
+    },
+    /// Get GT exchange vault.
+    GtExchangeVault { address: Option<Pubkey> },
+    /// GT Bank.
+    GtBank {
+        address: Option<Pubkey>,
+        #[arg(long, value_name = "GT_EXCHANGE_VAULT")]
+        vault: Option<Pubkey>,
+        #[clap(flatten)]
+        date: SelectGtExchangeVaultByDate,
+        #[arg(long)]
+        debug: bool,
     },
 }
 
@@ -213,11 +235,139 @@ impl super::Command for Gt {
                     }
                 }
             }
+            Command::GtExchangeVault { address } => {
+                let address = match address {
+                    Some(address) => *address,
+                    None => {
+                        let time_window = client.store(store).await?.gt.exchange_time_window;
+                        let time_window_index = current_time_window_index(time_window)?;
+                        client.find_gt_exchange_vault_address(store, time_window_index, time_window)
+                    }
+                };
+                let vault = client
+                    .account::<ZeroCopy<GtExchangeVault>>(&address)
+                    .await?
+                    .ok_or(gmsol_sdk::Error::NotFound)?
+                    .0;
+                println!("{vault:#?}");
+                return Ok(());
+            }
+            Command::GtBank {
+                address,
+                vault,
+                date,
+                debug,
+            } => {
+                let address = if let Some(address) = address {
+                    *address
+                } else {
+                    let vault = match vault {
+                        Some(vault) => *vault,
+                        None => date.get(store, client).await?,
+                    };
+                    let config = client.find_treasury_config_address(store);
+                    let config = client
+                        .account::<ZeroCopy<Config>>(&config)
+                        .await?
+                        .ok_or(gmsol_sdk::Error::NotFound)?
+                        .0;
+                    let treasury_vault_config = optional_address(&config.treasury_vault_config)
+                        .ok_or_else(|| {
+                            gmsol_sdk::Error::custom("treasury vault config is not set")
+                        })?;
+                    client.find_gt_bank_address(treasury_vault_config, &vault)
+                };
+
+                let bank = client
+                    .account::<ZeroCopy<GtBank>>(&address)
+                    .await?
+                    .ok_or(gmsol_sdk::Error::NotFound)?
+                    .0;
+                if *debug {
+                    println!("{bank:#?}");
+                } else {
+                    println!("Address: {address}");
+                    println!("Treasury Vault Config: {}", bank.treasury_vault_config);
+                    println!("GT Exchange Vault: {}", bank.gt_exchange_vault);
+                    let mut status = String::default();
+                    let is_confirmed = bank.flags.get_flag(GtBankFlags::Confirmed);
+                    if is_confirmed {
+                        status.push_str("confirmed");
+                    } else {
+                        status.push_str("not confirmed");
+                    }
+                    if bank.flags.get_flag(GtBankFlags::SyncedAfterConfirmation) {
+                        status.push_str("synced");
+                    }
+                    println!("Status: {status}");
+
+                    let store = client.store(store).await?;
+                    let gt_decimals = store.gt.decimals;
+
+                    if is_confirmed {
+                        println!(
+                            "Remaining GT: {}",
+                            Amount::from_u64(bank.remaining_confirmed_gt_amount, gt_decimals)
+                        );
+                    }
+
+                    let token_map_address =
+                        optional_address(&store.token_map).ok_or_eyre("no authorized token map")?;
+                    let token_map = client.token_map(token_map_address).await?;
+                    println!("[Balances]");
+                    for (token, balance) in bank.balances.entries() {
+                        let token = Pubkey::new_from_array(*token);
+                        let token_decimals = token_map
+                            .get(&token)
+                            .ok_or(gmsol_sdk::Error::NotFound)?
+                            .token_decimals;
+                        let recevier_vault_out =
+                            Amount::from_u64(balance.receiver_vault_out, token_decimals);
+                        let balance = Amount::from_u64(balance.amount, token_decimals);
+
+                        println!(
+                            "{token}: balance = {}, receiver_vault_out = {}",
+                            balance, recevier_vault_out
+                        );
+                    }
+                }
+
+                return Ok(());
+            }
         };
 
         let bundle = txn.into_bundle_with_options(options)?;
 
         client.send_or_serialize(bundle).await?;
         Ok(())
+    }
+}
+
+#[derive(clap::Args, Clone, Debug)]
+pub(crate) struct SelectGtExchangeVaultByDate {
+    #[arg(long, short)]
+    date: Option<humantime::Timestamp>,
+}
+
+impl SelectGtExchangeVaultByDate {
+    pub(crate) async fn get<C: Deref<Target = impl Signer> + Clone>(
+        &self,
+        store: &Pubkey,
+        client: &gmsol_sdk::Client<C>,
+    ) -> gmsol_sdk::Result<Pubkey> {
+        use std::time::SystemTime;
+
+        let time_window = client.store(store).await?.gt.exchange_time_window;
+        let date = self
+            .date
+            .as_ref()
+            .cloned()
+            .unwrap_or_else(|| humantime::Timestamp::from(SystemTime::now()));
+        let ts = date
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .map_err(gmsol_sdk::Error::custom)?
+            .as_secs();
+        let index = ts / time_window as u64;
+        Ok(client.find_gt_exchange_vault_address(store, index as i64, time_window))
     }
 }
